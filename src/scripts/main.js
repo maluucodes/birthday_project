579f951af8a4fba134e4d97b0295fa8bb64081c4
Maria Luiza <m.luiza0020@gmail.com>
2026-02-12 00:00:06 -0300

    adicionando timer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('olá, mundo')
+AOS.init();
+
+const dataDoEvento = new Date("May 22, 2026 15:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaOTempo = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);;
+    const horasAteEvento = Math.floor(distanciaAteEvento % diaEmMs / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horaEmMs) / minutoEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutoEmMs) / 1000);
+
+    document.getElementById('timer').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaOTempo);
+        document.getElementById('timer').innerHTML = 'Já acabou :(';
+    }
+}, 1000);
